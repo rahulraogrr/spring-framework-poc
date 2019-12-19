77881c75907adb6f48063d2f040b84732a95ddcb
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2019-12-19 23:55:55 +0530

    Initial commit

---
+++ src/main/java/com/poc/SpringFrameworkPocApplication.java
@@ -0,0 +1,13 @@
+package com.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poc/SpringFrameworkPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
