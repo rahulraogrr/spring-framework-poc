7a31279a046bfcf7d299a1e0ba2f513cf885f015
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2019-12-21 09:10:30 +0530

    Framework Changes 4

---
+++ src/main/java/com/poc/constants/DemoConstants.java
@@ -0,0 +1,19 @@
+package com.poc.constants;
+
+/**
+ *
+ * @author Rahul Rao Gonda
+ *
+ */
+public final class DemoConstants {
+    private DemoConstants(){}
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+    public static final String SPRING_PROFILE_TEST = "test";
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+    public static final String SPRING_PROFILE_CLOUD = "cloud";
+    public static final String SPRING_PROFILE_HEROKU = "heroku";
+    public static final String SPRING_PROFILE_AWS_ECS = "aws-ecs";
+    public static final String SPRING_PROFILE_SWAGGER = "swagger";
+    public static final String SPRING_PROFILE_NO_LIQUIBASE = "no-liquibase";
+    public static final String SPRING_PROFILE_K8S = "k8s";
+}
