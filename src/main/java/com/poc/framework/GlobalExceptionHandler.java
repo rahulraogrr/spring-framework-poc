d495cc68e96bfe51ca63ed734e61ffad6686d264
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2019-12-25 23:33:49 +0530

    Framework Changes 9 GlobalExceptionHandler.java

--- src/main/java/com/poc/framework/GlobalExceptionHandler.java
+++ src/main/java/com/poc/framework/GlobalExceptionHandler.java
@@ -10,9 +10,24 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import javax.servlet.http.HttpServletRequest;
 import java.time.ZonedDateTime;
 
+/**
+ * <p>Handles all the exceptions thrown in the application and returns
+ * {@link ApiErrorResponse}</p>
+ * @author Rahul Rao Gonda
+ * @since 1.0.0
+ */
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    /**
+     * <p>Handles {@link ResourceNotFoundException} Exception</p>
+     * @param e {@link ResourceNotFoundException}
+     * @param request {@link HttpServletRequest}
+     * @return {@link ApiErrorResponse}
+     *
+     * @see ResourceNotFoundException
+     * @since 1.0.0
+     */
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ApiErrorResponse> handleException(
             final ResourceNotFoundException e,
@@ -24,6 +39,15 @@ public class GlobalExceptionHandler {
                    e.getMessage()), HttpStatus.NOT_FOUND);
     }
 
+    /**
+     * <p>Handles {@link InvalidFieldException} Exception</p>
+     * @param e {@link InvalidFieldException}
+     * @param request {@link HttpServletRequest}
+     * @return {@link ApiErrorResponse}
+     *
+     * @see InvalidFieldException
+     * @since 1.0.0
+     */
     @ExceptionHandler(InvalidFieldException.class)
     public ResponseEntity<ApiErrorResponse> handleException(
             final InvalidFieldException e,
@@ -36,6 +60,14 @@ public class GlobalExceptionHandler {
                 e.getMessage()), HttpStatus.BAD_REQUEST);
     }
 
+    /**
+     * <p>Handles any {@link Exception}</p>
+     * @param e {@link Exception}
+     * @param request {@link HttpServletRequest}
+     * @return {@link ApiErrorResponse}
+     *
+     * @since 1.0.0
+     */
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiErrorResponse> handleException(
             final Exception e,
