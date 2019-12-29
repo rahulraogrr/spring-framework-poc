e4f42886511dda0f8c1d633432848b70cff2aa78
Rahul Rao Gonda <rahulrao.grr@gmail.com>
2019-12-30 00:08:49 +0530

    Framework Changes 14 Added documentation for package-info.java

--- src/main/java/com/poc/constants/MessageConstants.java
+++ src/main/java/com/poc/constants/MessageConstants.java
@@ -7,11 +7,29 @@ package com.poc.constants;
  * @author Rahul Rao Gonda
  */
 public final class MessageConstants {
+	/**
+	 * To be used when you don't find the resource sent from the frontend
+	 */
 	public static final String RESOURCE_NOT_FOUND = "Resource Not Found";
+	/**
+	 * To be used when a resource is being modified
+	 */
 	public static final String RESOURCE_MODIFY_SUCCESS = "Resource Modified Successfully";
+	/**
+	 * To be used when a resource is being deleted
+	 */
 	public static final String RESOURCE_DELETE_SUCCESS = "Resource Deleted Successfully";
+	/**
+	 * Message Separate
+	 */
 	public static final String MESSAGE_SEPARATE = "~";
+	/**
+	 * To be used to create a new resource
+	 */
 	public static final String RESOURCE_CREATE_SUCCESS = "Resource Created Successfully";
+	/**
+	 * To be used when we authorise a resource
+	 */
 	public static final String RESOURCE_AUTHORISE_SUCCESS = "Resource Authorised Successfully";
 	private MessageConstants(){}
 }
